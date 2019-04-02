4ec94f2019a93032f9cb9648f5d44d25641a24ee
Samuel1418 <sgomezpintos@danielcastelao.org>
2019-04-02 10:31:05 +0200

    Acabado

---
+++ src/boletin25/Boletin25.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Para ver la segunda interfaz, se tiene que dar al boton register
+        Interfaz2 obx2 = new Interfaz2();
+        obx2.setVisible(true);
+    }
+    
+}
